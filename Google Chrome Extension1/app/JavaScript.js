9b2ee935a0d42bbc7e3d6cd14cfc883e80db4fa7
Rakeen Huq <rakeenhuq@hotmail.com>
2017-07-27 23:14:00 -0700

    added first project

---
+++ Google Chrome Extension1/app/JavaScript.js
@@ -0,0 +1,81 @@
+﻿
+//Wait for the document(popup.html) to fully load before executing any javascript
+document.addEventListener('DOMContentLoaded', function ()
+{
+    var button = document.getElementById('button1');
+    // onClick's logic below:
+    button.addEventListener('click', function ()
+    {
+        //Clear any previously stored data
+        chrome.storage.local.clear(function (callback)
+        {
+            //If there was an error clearing local storage.
+            var error = chrome.runtime.lastError;
+            if (error)
+            {
+                console.error(error);
+            }
+            else
+            {
+                console.log("local storage cleared");
+            }
+        });
+
+        //Now get the specified url.
+        url = getValue();
+
+        //Get the JSON data by using the specified url
+        getJSON(url);
+
+        //Store the url in local storage and set the alarm to activate every 1 minute
+        chrome.storage.local.set({ "url": url });
+        chrome.alarms.create("alarm", { delayInMinutes: 1, periodInMinutes: 1 });
+    });
+});
+
+//This function obtains the url from user input and adds the ".json" tag to the end of the url
+function getValue()
+{
+    var val = document.getElementById('text').value + ".json"; //obtain user input from html
+    return val;
+
+    //url(val + ".json"); //cals the function "url" and adds ".json" tag to end of user specified url
+}
+
+
+//This function takes in a specified url with proper ".json" tag at the end of the url and proceeds to get JSON data back from the server
+function getJSON(url)
+{
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.send();
+    req.onreadystatechange = function ()
+    {
+        if (this.readyState === 4 && this.status === 200)
+        {
+            var jsonData = JSON.parse(this.responseText);
+            parseJSON(jsonData); //if data retreival is successful, call "parseJSON" function 
+        }
+    };
+}
+
+//This function parses the JSON data retrieved from the server and stores it all in an array
+function parseJSON(jsonData)
+{
+    var dataStorage = {}; //create an array to hold information from reddit
+
+    //now store the information obtained from the URL array, looping through all 25(default value) posts
+    for (var i = 0; i < 25; i++)
+    {
+
+        //get the title and url of each post
+        var title = JSON.stringify(jsonData.data.children[i].data.title);
+        var url = JSON.stringify(jsonData.data.children[i].data.url);
+        var thumbnail = JSON.stringify(jsonData.data.children[i].data.thumbnail);
+        //store the title and its respective url in each index of the array
+        dataStorage[title] = url;
+
+    }
+
+    chrome.storage.local.set({ data: dataStorage });
+}
---
+++ Google Chrome Extension1/app/background.js
@@ -0,0 +1,107 @@
+﻿//Listen for alarm every 1 minute
+chrome.alarms.onAlarm.addListener(function (getJSON)
+{
+    //Get the specified URL stored in local storage
+    chrome.storage.local.get("url", function (obj)
+    {
+        var req = new XMLHttpRequest();
+        req.open("GET", obj.url, true);
+        req.send();
+        req.onreadystatechange = function ()
+        {
+            if (this.readyState === 4 && this.status === 200)
+            {
+                var jsonData = JSON.parse(this.responseText);
+                parseJSON(jsonData); //if data retreival is successful, call "parseJSON" function 
+            }
+        };
+    });
+});
+
+//This function parses the JSON data retrieved from the server and stores it all in an array
+function parseJSON(jsonData)
+{
+    var dataStorage = {}; //create an array to hold information from reddit
+    chrome.storage.local.get("data", function (obj)
+    {
+        //now store the information obtained from the URL array, looping through all 25(default value) posts
+        for (var i = 0; i < 25; i++)
+        {
+            //get the title and url of each post
+            var title = JSON.stringify(jsonData.data.children[i].data.title);
+            var url = JSON.stringify(jsonData.data.children[i].data.url);
+            var thumbnail = JSON.stringify(jsonData.data.children[i].data.thumbnail);
+
+            //if user has already been notified, i.e. there is collision in the table, keep track of item in new table
+            if (obj.data[title] === url)
+            {
+                dataStorage[title] = url;
+                console.log("item already in dataStorage");
+            }
+                //if user has not been notified and item was not in table, i.e. no collision, add item to new table and notify user
+            else if (obj.data[title] === undefined)
+            {
+                dataStorage[title] = url;
+                console.log("adding item to dataStorage");
+                console.log(title);
+                notification(thumbnail, url, title);
+            }
+
+            //if anything else is in the table and there was no collision, it is to be removed from the table.
+            //No actual removal occurs, just a new table is created and the old items are no longer in the new table.
+        }
+        console.log(dataStorage);
+        chrome.storage.local.set({ data: dataStorage });
+    });
+
+}
+
+
+//creates a notification on the desktop
+function notification(thumbnail, url, title)
+{
+  
+    chrome.notifications.create({
+        type: "basic",
+        title: "New Posts Found",
+        message: title,
+        iconUrl: "icon.png",
+        //buttons: [{ title: "Go to product" }],
+        isClickable: true,
+        requireInteraction: true
+    });
+
+}
+
+//User may click on the notification to take them to where the product sale was posted
+chrome.notifications.onClicked.addListener(function (replyBtnClick)
+{
+    chrome.storage.local.get("url", function (obj)
+    {
+        obj.url = (obj.url).replace(".json", "");
+        chrome.tabs.create({ url: obj.url });
+    });
+
+});
+
+//chrome.notifications.onButtonClicked.addListener(function (replyBtnClick)
+//{
+//    chrome.storage.local.get("notificationURL", function (obj)
+//    {
+//        var temp = document.createElement("p");
+//        temp.appendChild(textURL);
+//        var temp2 = document.getElementsByTagName("p");
+//        console.log("temp2 : " + temp2);
+//        var req = new XMLHttpRequest();
+//        req.open("GET", temp2, true);
+//        req.responseType = 'document';
+//        req.onload = function(e)
+//        {
+//            console.log("e: " + e.responseURL);
+//            console.log("this: " + this.responseURL);
+//            chrome.tabs.create({ url: e.responseURL });
+//        };
+
+//        req.send();
+//    });
+//});
